8d6cc322d0fabc2a9257d94c0620c4695b758c86
Samatar Moussa <samatarmoussa111@gmail.com>
2022-08-24 12:20:48 +0300

    création des composants Checkout et Subtotal

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
 const Checkout = () => {
-  return <div>Checkout</div>;
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAQEy4Rt240oUWfYrFUNSlPN1bIuM45kNmsVgyKzdI3lpq-u-TcEP01n4TD9gcLLeNaQ&usqp=CAU"
+          alt="amazon ads"
+        />
+        <div>
+          <h2 className="checkout__title">Your shopping Basket</h2>
+          {/** Basket item */}
+          {/** Basket item */}
+          {/** Basket item */}
+        </div>
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
