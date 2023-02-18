bae0d4b2c442c62f3136085e97fde05f300bb131
Samatar Moussa <samatarmoussa111@gmail.com>
2023-02-18 18:14:13 +0300

    Creation du composant Login partie front

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Checkout from "./pages/Checkout/Checkout";
-import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -3,28 +3,32 @@ import "./Checkout.css";
 import Subtotal from "../../components/Subtotal/Subtotal";
 import { useStateValue } from "../../contexts/BasketContext";
 import CheckoutProduct from "../../components/CheckoutProduct/CheckoutProduct";
+import Header from "../../components/Header/Header";
 
 const Checkout = () => {
   const [{ basket }, dispatch] = useStateValue();
   return (
-    <div className="checkout">
-      <div className="checkout__left">
-        <img
-          className="checkout__ad"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAQEy4Rt240oUWfYrFUNSlPN1bIuM45kNmsVgyKzdI3lpq-u-TcEP01n4TD9gcLLeNaQ&usqp=CAU"
-          alt="amazon ads"
-        />
-        <div>
-          <h2 className="checkout__title">Your shopping Basket</h2>
-          {basket.map((item, k) => {
-            return <CheckoutProduct key={k} item={item} />;
-          })}
+    <>
+      <Header />
+      <div className="checkout">
+        <div className="checkout__left">
+          <img
+            className="checkout__ad"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAQEy4Rt240oUWfYrFUNSlPN1bIuM45kNmsVgyKzdI3lpq-u-TcEP01n4TD9gcLLeNaQ&usqp=CAU"
+            alt="amazon ads"
+          />
+          <div>
+            <h2 className="checkout__title">Your shopping Basket</h2>
+            {basket.map((item, k) => {
+              return <CheckoutProduct key={k} item={item} />;
+            })}
+          </div>
+        </div>
+        <div className="checkout__right">
+          <Subtotal />
         </div>
       </div>
-      <div className="checkout__right">
-        <Subtotal />
-      </div>
-    </div>
+    </>
   );
 };
 
